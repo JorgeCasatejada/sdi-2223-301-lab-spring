516c13954fc1465c27501b6c27bde71b355b5506
Jorge Casatejada <uo282294@uniovi.es>
2023-02-16 16:32:13 +0100

    SDI-2223-301-2.2-Trabajando con Controladores.

---
+++ src/main/java/com/uniovi/sdi2223301spring/entities/Mark.java
@@ -0,0 +1,31 @@
+package com.uniovi.sdi2223301spring.entities;
+
+public class Mark {
+    private Long id;
+    private String email;
+    private Double score;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+}
