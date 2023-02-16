f3e0821997426ccece9d4f50e49eae157c438cf0
Jorge Casatejada <uo282294@uniovi.es>
2023-02-16 15:58:09 +0100

    SDI-2223-301-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi2223301spring/Sdi2223301SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223301spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2223301SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2223301SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi2223301spring/Sdi2223301SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2223301spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2223301SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
