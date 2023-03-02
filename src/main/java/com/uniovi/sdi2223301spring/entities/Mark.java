8e4c0fedd35c0dfcdf79f0f411c56a6a11c672e9
JorgeCasatejada <UO282294@uniovi.es>
2023-03-02 12:40:49 +0100

    SDI-2223-301-4.1-SpringBoot-Sesiones

--- src/main/java/com/uniovi/sdi2223301spring/entities/Mark.java
+++ src/main/java/com/uniovi/sdi2223301spring/entities/Mark.java
@@ -1,6 +1,7 @@
 package com.uniovi.sdi2223301spring.entities;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 public class Mark {
@@ -60,5 +61,12 @@ public class Mark {
 
     public void setUser(User user) {
         this.user = user;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mark mark = (Mark) o;
+        return Objects.equals(id, mark.id);
     }
 }
